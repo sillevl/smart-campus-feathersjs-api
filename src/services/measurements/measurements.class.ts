9ffc58ee34ec2b63dcf0b0565edc678c83805ed1
Sille Van Landschoot <info@sillevl.be>
2020-02-26 11:50:17 +0100

    remove unneeded actions

--- src/services/measurements/measurements.class.ts
+++ src/services/measurements/measurements.class.ts
@@ -14,33 +14,9 @@ export class Measurements implements ServiceMethods<Data> {
     this.app = app;
   }
 
-  async find (params?: Params): Promise<Data[] | Paginated<Data>> {
-    return [];
-  }
-
   async get (id: Id, params?: Params): Promise<Data> {
     return {
-      id, text: `A new message with ID: ${id}!`
+      id, text: `A new message with ID: ${id}!`, params
     };
-  }
-
-  async create (data: Data, params?: Params): Promise<Data> {
-    if (Array.isArray(data)) {
-      return Promise.all(data.map(current => this.create(current, params)));
-    }
-
-    return data;
-  }
-
-  async update (id: NullableId, data: Data, params?: Params): Promise<Data> {
-    return data;
-  }
-
-  async patch (id: NullableId, data: Data, params?: Params): Promise<Data> {
-    return data;
-  }
-
-  async remove (id: NullableId, params?: Params): Promise<Data> {
-    return { id };
   }
 }
